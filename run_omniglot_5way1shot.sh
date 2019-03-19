00f7458edbb0b2ed2477c1de2e2f10052f805035
Hae Beom Lee <43218718+haebeom-lee@users.noreply.github.com>
2019-03-20 07:09:18 +0900

    Update run_omniglot_5way1shot.sh

--- run_omniglot_5way1shot.sh
+++ run_omniglot_5way1shot.sh
@@ -1,8 +1,21 @@
 python main.py \
-  --savedir './results/run_omni_5way1shot' \
+  --savedir './results/omni_5way1shot' \
   --dataset 'omniglot' \
   --mode 'train' \
-  --gpu_num 7 \
+  --gpu_num 0 \
+  --alpha 0.4 \
+  --metabatch 32 \
+  --n_steps 1 \
+  --way 5 \
+  --shot 1 \
+  --query 15 \
+  --n_train_iters 40000
+
+python main.py \
+  --savedir './results/omni_5way1shot' \
+  --dataset 'omniglot' \
+  --mode 'test' \
+  --gpu_num 0 \
   --alpha 0.4 \
   --metabatch 32 \
   --n_steps 1 \
