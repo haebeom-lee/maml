f511886647bc2493089a41d7fed9907f85133010
Hae Beom Lee <43218718+haebeom-lee@users.noreply.github.com>
2019-03-20 07:08:41 +0900

    Update run_omniglot_20way1shot.sh

--- run_omniglot_20way1shot.sh
+++ run_omniglot_20way1shot.sh
@@ -1,8 +1,21 @@
 python main.py \
-  --savedir './results/run_omni_20way1shot' \
+  --savedir './results/omni_20way1shot' \
   --dataset 'omniglot' \
   --mode 'train' \
-  --gpu_num 6 \
+  --gpu_num 0 \
+  --alpha 0.1 \
+  --metabatch 16 \
+  --n_steps 5 \
+  --way 20 \
+  --shot 1 \
+  --query 15 \
+  --n_train_iters 40000
+
+python main.py \
+  --savedir './results/omni_20way1shot' \
+  --dataset 'omniglot' \
+  --mode 'test' \
+  --gpu_num 0 \
   --alpha 0.1 \
   --metabatch 16 \
   --n_steps 5 \
