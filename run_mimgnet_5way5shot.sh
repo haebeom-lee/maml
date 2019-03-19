62dbe5e430bfcd8e64f78bee5e1d40b5aa3e50d9
Hae Beom Lee <43218718+haebeom-lee@users.noreply.github.com>
2019-03-20 07:08:07 +0900

    Update run_mimgnet_5way5shot.sh

--- run_mimgnet_5way5shot.sh
+++ run_mimgnet_5way5shot.sh
@@ -1,8 +1,8 @@
 python main.py \
-  --savedir './results/run_mimgnet_5way5shot_lowlr_goodinit' \
+  --savedir './results/mimgnet_5way5shot' \
   --dataset 'mimgnet' \
   --mode 'train' \
-  --gpu_num 6 \
+  --gpu_num 0 \
   --alpha 0.01 \
   --metabatch 4 \
   --n_steps 5 \
@@ -10,3 +10,17 @@ python main.py \
   --shot 5 \
   --query 15 \
   --meta_lr 1e-4
+  
+  python main.py \
+  --savedir './results/mimgnet_5way5shot' \
+  --dataset 'mimgnet' \
+  --mode 'test' \
+  --gpu_num 0 \
+  --alpha 0.01 \
+  --metabatch 4 \
+  --n_steps 10 \
+  --way 5 \
+  --shot 5 \
+  --query 15 \
+  --meta_lr 1e-4
+
